e4d5fd40be40ec057c8967b6386346f99a9f2bf6
SALMAN MUHAMMAD KHAN <k250017@nu.edu.pk>
2025-12-03 20:49:03 +0500

    Create Q4.c
    Allocate and initialize an array, print values, and demonstrate memory allocation with calloc and malloc.

---
+++ Q4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int *arr = (int *)calloc(n, sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        arr[i] = i + 1;
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    free(arr);
+
+    arr = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        arr[i] = (i + 1) * 10;
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
